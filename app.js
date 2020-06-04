985dd352f4f0be006328f93678fd71e0f5143fea
Sam Joyce <samjoycesamjoyce@Sams-MBP.home>
2020-06-04 09:33:07 +0100

    map added to page using Mapbox SDK

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+import 'https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.js';
+
+
+// Call function upon window loading
+
 window.onload = () => {
   getCovidStatistics();
 };
+
+// Counter of stats
 
 function getCovidStatistics() {
   fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/223')
@@ -18,3 +25,26 @@ function getCovidStatistics() {
 
     setTimeout(getCovidStatistics, 43200000);
 }
+
+// Mapbox
+
+const token = 'pk.eyJ1Ijoic2Ftam95Y2UiLCJhIjoiY2tiMGN3ZnlrMDZ1dDJ1bnY4amJhODJoMSJ9.J0w-aoTv7yze_o80EAK-uw';
+
+mapboxgl.accessToken = token;
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/dark-v10',
+zoom: 1.5
+});
+
+function makeCovidMap() {
+  fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/')
+    .then(res => res.json())
+    .then(data => {
+      const locations  = data.locations;
+      locations.forEach(location => 
+        console.log(location));
+    })
+
+}
+makeCovidMap();
